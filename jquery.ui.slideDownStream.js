246f9107ba4dff02622c4e02404a8932cdca6fe4
hokaccha <k.hokamura@gmail.com>
2010-10-19 17:54:59 +0900

    create project

---
+++ jquery.ui.slideDownStream.js
@@ -0,0 +1,91 @@
+/*
+ * jquery ui slideDownStream
+ *
+ * Copyright (c) 2010 Kazuhito Hokamura
+ * Licensed under the MIT License:
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * @author   Kazuhito Hokamura (http://webtech-walker.com/)
+ * @version  0.0.1
+ *
+ */
+
+$.widget('ui.slideDownStream', {
+    _queue: [],
+    _state: 'stop',
+
+    options: {
+        slideDownSpeed: 300,
+        intervalTime: 200,
+        autoStart: true,
+        initList: null,
+        startHandler: function() {},
+        waitHandler: function() {},
+        addHandler: function() {}
+    },
+
+    _init: function() {
+        var self = this;
+
+        if (self.options.defaultList) {
+            $.each(this.options.defaultList, function(idx, item) {
+                self.element.prepend(item);
+            });
+        }
+
+        if (self.options.autoStart) {
+            self._state = 'wait';
+        }
+        
+        return self;
+    },
+    push_queue: function(items) {
+        var self = this;
+
+        $.each(items, function(idx, item) {
+            self._queue.push( $(item).hide() );
+        });
+
+        if (self._state == 'wait') self.start();
+
+        return self;
+    },
+    clear_queue: function() {
+        var self = this;
+
+        self._queue = [];
+
+        return self;
+    },
+    start: function() {
+        var self = this;
+
+        self._state = 'process';
+        self.options.startHandler.call(self.element);
+        (function loop() {
+            if (self._state != 'process') return;
+
+            var item = self._queue.shift();
+            if (item) {
+                self.element.prepend(item);
+                item.slideDown(self.options.slideDownSpeed, function() {
+                    self.options.addHandler.call(self.element, item);
+                    setTimeout(loop, self.options.intervalTime);
+                });
+            }
+            else {
+                self._state = 'wait';
+                self.options.waitHandler.call(self.element);
+            }
+        })();
+
+        return self;
+    },
+    stop: function() {
+        var self = this;
+
+        self._state = 'stop';
+
+        return self;
+    },
+});
---
+++ test/basic.t.js
@@ -0,0 +1,30 @@
+var container = $('<ul>');
+var fixture = $('#qunit-fixture');
+
+container.slideDownStream({
+    defaultList: $('#default_list').find('li'),
+    addHandler: function() {},
+    waitHandler: function() {},
+    autoStart: false
+});
+
+var s = container.data('slideDownStream');
+s.push_queue( $('#push_list').find('li') );
+
+test("basic", function() {
+    ok('slideDownStream' in container, 'container has slideDownStream');
+    equals(container.find('li').size(), 5, 'list item size is 5');
+    equals(container.find('li').eq(0).text(), 'item5', 'first item value is item5');
+    equals(s._state, 'stop', 'state is stop');
+    equals(s._queue.length, 3, 'queue length is 3');
+});
+
+asyncTest("asinc test", function() {
+    s.options.waitHandler = function() {
+        start();
+        equals(s._state, 'wait', 'state is wait');
+        equals(container.find('li').size(), 8, 'list item size is 8');
+        equals(container.find('li').eq(0).text(), 'item8', 'first item value is item8');
+    };
+    s.start();
+});
